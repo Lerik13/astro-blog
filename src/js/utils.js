0ecc1a44c50ea53c27d064fc0c3e128d80b8372b
Valeria <lerik13@gmail.com>
2023-06-13 16:34:13 -0600

    + utils function: formatBlogPosts - filter posts by draft, futureDate, limit & sorting by date or randomize posts

--- src/js/utils.js
+++ src/js/utils.js
@@ -14,3 +14,39 @@ export function formatDate(date){
 		timeZone: "UTC",
 	})
 }
+
+export function formatBlogPosts(posts, {
+	filterOutDrafts = true,
+	filterOutFuturePosts = true,
+	sortByDate = true,
+	limit = undefined,
+} = {} ) {
+	
+	const filteredPosts = posts.reduce((acc, post) => {
+		const { date, draft } = post.frontmatter;
+		// filterOtDrafts if true
+		if (filterOutDrafts && draft) return acc;
+		
+		// filterOutFuturePosts if true
+		if (filterOutFuturePosts && new Date(date) > new Date()) return acc;
+
+		// add post to acc
+		acc.push(post);
+
+		return acc;
+
+	}, [])
+
+	if (sortByDate) {
+		filteredPosts.sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date))
+	} else {
+		// Randomize posts
+		filteredPosts.sort(() => Math.random() - 0.5)
+	}
+
+	// limit if number is passed
+	if (typeof limit === "number") {
+		return filteredPosts.slice(0, limit)
+	}
+	return filteredPosts;
+}
